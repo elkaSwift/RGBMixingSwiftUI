bbe2743b36105786f3a4a35683303b8ebdc60f5f
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-20 15:49:50 +0300

    Creating an additional file in which the ImageView settings will be stored

--- RGBMixingSwiftUI/ImageView.swift
+++ RGBMixingSwiftUI/ImageView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ImageView.swift
 //  RGBMixingSwiftUI
 //
 //  Created by Елисей Мищенко on 20.02.2022.
@@ -7,16 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ImageView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ImageView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ImageView()
     }
 }
