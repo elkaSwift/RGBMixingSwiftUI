e2865392f6a7240d7d0587457c685ba8cf15b7ed
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-20 21:03:00 +0300

    Creating two ColorTextView and ColorTextFieldView files in which text and textFields will be configured

--- RGBMixingSwiftUI/ColorTextFieldView.swift
+++ RGBMixingSwiftUI/ColorTextFieldView.swift
@@ -1,5 +1,5 @@
 //
-//  ColorSlider.swift
+//  ColorTextField.swift
 //  RGBMixingSwiftUI
 //
 //  Created by Елисей Мищенко on 20.02.2022.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ColorSlider: View {
+struct ColorTextFieldView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ColorSlider_Previews: PreviewProvider {
+struct ColorTextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        ColorSlider()
+        ColorTextFieldView()
     }
 }
