afbf18a3d896087c22b9f149d1e1998f1076d34c
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-20 15:39:30 +0300

    Initial Commit

---
+++ RGBMixingSwiftUI/RGBMixingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBMixingSwiftUIApp.swift
+//  RGBMixingSwiftUI
+//
+//  Created by Елисей Мищенко on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBMixingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
