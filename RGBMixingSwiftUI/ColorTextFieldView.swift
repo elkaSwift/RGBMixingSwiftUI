ae6524ba48b79c96de2e45940b32c2bd59def7c7
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-22 19:28:43 +0300

    Wrote logic and created animation for a text field

--- RGBMixingSwiftUI/ColorTextFieldView.swift
+++ RGBMixingSwiftUI/ColorTextFieldView.swift
@@ -8,15 +8,40 @@
 import SwiftUI
 
 struct ColorTextFieldView: View {
-    @State private var textValue = ""
-    @Binding private var value: Double
+    @Binding var textValue: String
+    @Binding var value: Double
+    
+    @State private var showAlert = false
+    
     var body: some View {
-        TextField(textValue: $textValue, value: $value)
+        TextField("", text: $textValue) { _ in
+            withAnimation {
+                
+            }
+        }
+        .frame(width: 50, alignment: .trailing)
+        .textFieldStyle(.roundedBorder)
+        .keyboardType(.decimalPad)
+        .alert("Wrong Format", isPresented: $showAlert, actions: {}) {
+            Text("Enter a value from 0 to 255")
+        }
+    }
+}
+
+extension ColorTextFieldView {
+    private func checkValue() {
+        if let value = Int(textValue), (0...255).contains(value) {
+            self.value = Double(value)
+            return
+        }
+        showAlert.toggle()
+        textValue = "0"
+        value = 0
     }
 }
 
 struct ColorTextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        ColorTextFieldView()
+        ColorTextFieldView(textValue: .constant("0"), value: .constant(0.0))
     }
 }
