f9f289b6002b295d31c83c0a33da8d3b05874403
Elisei Mishchenko <esm.mail@icloud.com>
2022-02-22 20:00:36 +0300

    I have created a main screen on which I have placed the colorView, sliders, as well as textFields and text, but it is not working at the moment

--- RGBMixingSwiftUI/ContentView.swift
+++ RGBMixingSwiftUI/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var red = Double.random(in: 0...255)
+    @State private var green = Double.random(in: 0...255)
+    @State private var blue = Double.random(in: 0...255)
+    
+    @FocusState private var focusedField: Bool
+    
     var body: some View {
-        VStack {
-//            ColorView(color: Color(red: <#T##Double#>, green: <#T##Double#>, blue: <#T##Double#>))
+        ZStack {
             
-            ColorSlider(color: .red, sliderValue: .constant(255))
-            ColorSlider(color: .green, sliderValue: .constant(255))
-            ColorSlider(color: .blue, sliderValue: .constant(255))
-            
-        }
         
+            VStack(spacing: 40){
+               ColorView(red: red, green: green, blue: blue)
+                VStack {
+                    ColorSliderView(sliderValue: $red, color: .red)
+                    ColorSliderView(sliderValue: $green, color: .green)
+                    ColorSliderView(sliderValue: $blue, color: .blue)
+                }
+                .frame(height: 150)
+                .focused($focusedField)
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer()
+                        Button("Done") {
+                            focusedField = false
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
